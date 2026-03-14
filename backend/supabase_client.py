0fd0ab3d8309e1c3930e1508d465db1c0d39e14e
Tykiu <quang26012007@gmail.com>
2026-03-14 18:21:51 +0700

    Delete backend directory

--- backend/supabase_client.py
+++
@@ -1,6 +0,0 @@
-from supabase import create_client, Client
-
-SUPABASE_URL = "https://zfpfyuyegzzzvyxfdwxe.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpmcGZ5dXllZ3p6enZ5eGZkd3hlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3Mjg4MTE2MSwiZXhwIjoyMDg4NDU3MTYxfQ.atxi4CZC2NY-qh_VOup8wfLOhqHeF-xdwIKX8X9OzR8"
-
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
